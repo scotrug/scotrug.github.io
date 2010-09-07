98749b8b21ecc3a83b2013045434177cea06508b
Paul Wilson <paulanthonywilson@gmail.com>
2010-09-07 15:59:07 +0100

    nothing much

--- config.ru
+++ config.ru
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'sinatra'
 require 'scotrug'
 
 run Sinatra::Application
--- scotrug.rb
+++ scotrug.rb
@@ -1,5 +1,9 @@
-require 'rubygems'
 require 'sinatra'
+
+before do
+  response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
+end
+
 
 set :public, Proc.new { File.join(root, "_site") }
 
